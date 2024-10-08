b926d1348c1e00259879236ca26b0ba7061209ef
deathnotelol <sawnaylinn2017@gmail.com>
2024-10-08 23:33:47 +0630

    update

--- index.php
+++ index.php
@@ -1,202 +1,209 @@
-
 <?php
 session_start();
 error_reporting(0);
-include('includes/config.php');
-if(isset($_POST['signin']))
-{
-$uname=$_POST['username'];
-$password=md5($_POST['password']);
-$sql ="SELECT EmailId,Password,Status,id FROM tblemployees WHERE EmailId=:uname and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
- foreach ($results as $result) {
-    $status=$result->Status;
-    $_SESSION['eid']=$result->id;
-  } 
-if($status==0)
-{
-$msg="Your account is Inactive. Please contact admin";
-} else{
-$_SESSION['emplogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location = 'employee/dashboard.php'; </script>";
-} }
+include('employee/includes/config.php');
+if (isset($_POST['signin'])) {
+    $uname = $_POST['username'];
+    $password = md5($_POST['password']);
+    $sql = "SELECT EmailId,Password,Status,id FROM tblemployees WHERE EmailId=:uname and Password=:password";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':uname', $uname, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+    if ($query->rowCount() > 0) {
+        foreach ($results as $result) {
+            $status = $result->Status;
+            $_SESSION['eid'] = $result->id;
+        }
+        if ($status == 0) {
+            $msg = "Your account is Inactive. Please contact admin";
+        } else {
+            $_SESSION['emplogin'] = $_POST['username'];
+            echo "<script type='text/javascript'> document.location = 'employee/dashboard.php'; </script>";
+        }
+    } else {
 
-else{
-  
-  echo "<script>alert('Invalid Details');</script>";
-
+        echo "<script>alert('Invalid Details');</script>";
+    }
 }
 
-}
-
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        
-        <!-- Title -->
-        <title> HRMS | Home Page</title>
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-        <meta charset="UTF-8">
-        <meta name="description" content="Responsive Admin Dashboard Template" />
-        <meta name="keywords" content="admin,dashboard" />
-        <meta name="author" content="Steelcoders" />
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!-- Styles -->
-        <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css"/>
-             <link href="assets/css/materialdesign.css" rel="stylesheet">
-        <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">        
 
-        	
-        <!-- Theme Styles -->
-        <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
-        
-        
-    </head>
-    <body>
-        <div class="loader-bg"></div>
-        <div class="loader">
-            <div class="preloader-wrapper big active">
-                <div class="spinner-layer spinner-blue">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
+<head>
+
+    <!-- Title -->
+    <title> HRMS | Home Page</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta charset="UTF-8">
+    <meta name="description" content="Responsive Admin Dashboard Template" />
+    <meta name="keywords" content="admin,dashboard" />
+    <meta name="author" content="Steelcoders" />
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css" />
+    <link href="assets/css/materialdesign.css" rel="stylesheet">
+    <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">
+
+
+    <!-- Theme Styles -->
+    <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+    <div class="loader-bg"></div>
+    <div class="loader">
+        <div class="preloader-wrapper big active">
+            <div class="spinner-layer spinner-blue">
+                <div class="circle-clipper left">
                     <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                    </div>
                 </div>
-                <div class="spinner-layer spinner-spinner-teal lighten-1">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
+                <div class="gap-patch">
                     <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                    </div>
                 </div>
-                <div class="spinner-layer spinner-yellow">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
+                <div class="circle-clipper right">
                     <div class="circle"></div>
-                    </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                    </div>
                 </div>
-                <div class="spinner-layer spinner-green">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div><div class="gap-patch">
+            </div>
+            <div class="spinner-layer spinner-spinner-teal lighten-1">
+                <div class="circle-clipper left">
                     <div class="circle"></div>
-                    </div><div class="circle-clipper right">
+                </div>
+                <div class="gap-patch">
                     <div class="circle"></div>
-                    </div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+            <div class="spinner-layer spinner-yellow">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+            <div class="spinner-layer spinner-green">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
                 </div>
             </div>
         </div>
-        <div class="mn-content fixed-sidebar">
-            <header class="mn-header navbar-fixed">
-                <nav class="cyan darken-1">
-                    <div class="nav-wrapper row">
-                        <section class="material-design-hamburger navigation-toggle">
-                            <a href="#" data-activates="slide-out" class="button-collapse show-on-large material-design-hamburger__icon">
-                                <span class="material-design-hamburger__layer"></span>
-                            </a>
-                        </section>
-                        <div class="header-title col s3">      
-                            <span class="chapter-title">HR Management System</span>
-                        </div>
-                      
-                           
-                        </form>
-                     
-                        
+    </div>
+    <div class="mn-content fixed-sidebar">
+        <header class="mn-header navbar-fixed">
+            <nav class="cyan darken-1">
+                <div class="nav-wrapper row">
+                    <section class="material-design-hamburger navigation-toggle">
+                        <a href="#" data-activates="slide-out" class="button-collapse show-on-large material-design-hamburger__icon">
+                            <span class="material-design-hamburger__layer"></span>
+                        </a>
+                    </section>
+                    <div class="header-title col s3">
+                        <span class="chapter-title">HR Management System</span>
                     </div>
-                </nav>
-            </header>
-           
-           
-            <aside id="slide-out" class="side-nav white fixed">
-                <div class="side-nav-wrapper">
-                   
-                  
-                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion" >
+
+
+                    </form>
+
+
+                </div>
+            </nav>
+        </header>
+
+
+        <aside id="slide-out" class="side-nav white fixed">
+            <div class="side-nav-wrapper">
+
+
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
                     <li>&nbsp;</li>
                     <li class="no-padding"><a class="waves-effect waves-grey" href="index.php"><i class="material-icons">account_box</i>Employe Login</a></li>
                     <li class="no-padding"><a class="waves-effect waves-grey" href="employee/forgot-password.php"><i class="material-icons">account_box</i>Emp Password Recovery</a></li>
-                
-                       <li class="no-padding"><a class="waves-effect waves-grey" href="admin/"><i class="material-icons">account_box</i>Admin Login</a></li>
-                
+
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="admin/"><i class="material-icons">account_box</i>Admin Login</a></li>
+
                 </ul>
-          <div class="footer">
+                <div class="footer">
                     <p class="copyright">ELMS ©</p>
-                
-                </div>
-                </div>
-            </aside>
-            <main class="mn-inner">
-                <div class="row">
-                    <div class="col s12">
-                        <div class="page-title"><h4>Welcome to HR Management System</h4></div>
 
-                          <div class="col s12 m6 l8 offset-l2 offset-m3">
-                              <div class="card white darken-1">
+                </div>
+            </div>
+        </aside>
+        <main class="mn-inner">
+            <div class="row">
+                <div class="col s12">
+                    <div class="page-title">
+                        <h4>Welcome to HR Management System</h4>
+                    </div>
 
-                                  <div class="card-content ">
-                                      <span class="card-title" style="font-size:20px;">Employee Login</span>
-                                         <?php if($msg){?><div class="errorWrap"><strong>Error</strong> : <?php echo htmlentities($msg); ?> </div><?php }?>
-                                       <div class="row">
-                                           <form class="col s12" name="signin" method="post">
-                                               <div class="input-field col s12">
-                                                   <input id="username" type="text" name="username" class="validate" autocomplete="off" required >
-                                                   <label for="email">Email Id</label>
-                                               </div>
-                                               <div class="input-field col s12">
-                                                   <input id="password" type="password" class="validate" name="password" autocomplete="off" required>
-                                                   <label for="password">Password</label>
-                                               </div>
-                                               <div class="col s12 right-align m-t-sm">
-                                                
-                                                   <input type="submit" name="signin" value="Sign in" class="waves-effect waves-light btn teal">
-                                               </div>
-                                           </form>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
+                    <div class="col s12 m6 l8 offset-l2 offset-m3">
+                        <div class="card white darken-1">
+
+                            <div class="card-content ">
+                                <span class="card-title" style="font-size:20px;">Employee Login</span>
+                                <?php if ($msg) { ?><div class="errorWrap"><strong>Error</strong> : <?php echo htmlentities($msg); ?> </div><?php } ?>
+                                <div class="row">
+                                    <form class="col s12" name="signin" method="post">
+                                        <div class="input-field col s12">
+                                            <input id="username" type="text" name="username" class="validate" autocomplete="off" required>
+                                            <label for="email">Email Id</label>
+                                        </div>
+                                        <div class="input-field col s12">
+                                            <input id="password" type="password" class="validate" name="password" autocomplete="off" required>
+                                            <label for="password">Password</label>
+                                        </div>
+                                        <div class="col s12 right-align m-t-sm">
+
+                                            <input type="submit" name="signin" value="Sign in" class="waves-effect waves-light btn teal">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </main>
-            
-        </div>
-        <div class="left-sidebar-hover"></div>
-        
-        <!-- Javascripts -->
-        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
-        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
-        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
-        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
-        <script src="assets/plugins/waypoints/jquery.waypoints.min.js"></script>
-        <script src="assets/plugins/counter-up-master/jquery.counterup.min.js"></script>
-        <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
-        <script src="assets/plugins/chart.js/chart.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.time.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.symbol.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.resize.min.js"></script>
-        <script src="assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
-        <script src="assets/plugins/curvedlines/curvedLines.js"></script>
-        <script src="assets/plugins/peity/jquery.peity.min.js"></script>
-        <script src="assets/js/alpha.min.js"></script>
-        <script src="assets/js/pages/dashboard.js"></script>
-        
-    </body>
+            </div>
+        </main>
+
+    </div>
+    <div class="left-sidebar-hover"></div>
+
+    <!-- Javascripts -->
+    <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+    <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+    <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+    <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+    <script src="assets/plugins/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/plugins/counter-up-master/jquery.counterup.min.js"></script>
+    <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+    <script src="assets/plugins/chart.js/chart.min.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.min.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.time.min.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.symbol.min.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.resize.min.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="assets/plugins/curvedlines/curvedLines.js"></script>
+    <script src="assets/plugins/peity/jquery.peity.min.js"></script>
+    <script src="assets/js/alpha.min.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+
+</body>
+
 </html>
